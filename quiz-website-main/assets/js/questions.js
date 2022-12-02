d43079f648c3f1d6fd00cc37059138a3fd4d7115
Anonymous0809 <119707842+Anonymous0809@users.noreply.github.com>
2022-12-03 02:18:22 +0400

    Add files via upload

---
+++ quiz-website-main/assets/js/questions.js
@@ -0,0 +1,127 @@
+var questionArr = [
+    {
+        // questionNumber: "Math-1",
+        question: "What is the name of Taylor Swift's first album?",
+        firstOption: "Midnights",
+        secondOption: "Lover",
+        thirdOption: "Reputation",
+        fourthOption: "Taylor Swift",
+        correctAnswer: "#fourth-option"
+    },
+    {
+        // questionNumber: "Math-2",
+        question: "Which of the following songs of Taylor swift's is more popular than the other options?",
+        firstOption: "Blank Space",
+        secondOption: "Lavender Haze",
+        thirdOption: "Cruel Summer",
+        fourthOption: "End game",
+        correctAnswer: "#first-option"
+    },
+    {
+        // questionNumber: "Math-3",
+        question: "Which song of taylor swift's did you first add to your playlist, 'T-Swift'? ",
+        firstOption: "Wildest Dreams",
+        secondOption: "We are never ever getting back together",
+        thirdOption: "willow",
+        fourthOption: "Lover",
+        correctAnswer: "#second-option"
+    },
+    {
+        // questionNumber: "Math-4",
+        question: "What is the square of the number of Spotify followers you have?",
+        firstOption: "225",
+        secondOption: "169",
+        thirdOption: "155",
+        fourthOption: "144",
+        correctAnswer: "#second-option"
+    },
+    {
+        // questionNumber: "Math-5",
+        question: "How many people do you follow on Spotify?",
+        firstOption: "17",
+        secondOption: "19",
+        thirdOption: "12",
+        fourthOption: "20",
+        correctAnswer: "#second-option"
+    },
+  
+    {
+        // questionNumber: "English-1",
+        question: "Who is the lead singer of the band sharing the name as the one your parents wanted to give you? ",
+        firstOption: "Dave Grohl",
+        secondOption: "Michael Jackson",
+        thirdOption: "Adam Levine" ,
+        fourthOption: "Kurt Cobain",
+        correctAnswer: "#fourth-option"
+    },
+    {
+        // questionNumber: "English-2",
+        question: "In what song does Taylor sing: “She wears high heels, I wear sneakers…”?",
+        firstOption: "Shake it off",
+        secondOption: "You will never be alone",
+        thirdOption: "You belong with me",
+        fourthOption: "Cruel Summer",
+        correctAnswer: "#third-option"
+    },
+    {
+        // questionNumber: "English-3",
+        question: "Which color have you most prominently wore in your highlight, 'fit check', be it a top or bottom",
+        firstOption: "black",
+        secondOption: "green",
+        thirdOption: "orange",
+        fourthOption: "blue",
+        correctAnswer: "#first-option"
+    },
+    {
+        // questionNumber: "English-4",
+        question: "At which non-school location did you, Misha and Pragya hang out alone for the first time?",
+        firstOption: "Your 2021 birthday bar",
+        secondOption: "Pragya's House",
+        thirdOption: "Mishas's House",
+        fourthOption: "Dubai Mall",
+        correctAnswer: "#third-option"
+    },
+   
+    {
+        // questionNumber: "History-1",
+        question: "What is your position in the football team?",
+        firstOption: "Left winger",
+        secondOption: "Striker",
+        thirdOption: "Right center back",
+        fourthOption: "Attacking midfielder",
+        correctAnswer: "#second-option"
+    },
+    {
+        // questionNumber: "History-2",
+        question: "What is your favorite song?",
+        firstOption: "Hundred Miles",
+        secondOption: "Woman",
+        thirdOption: "Give me everything",
+        fourthOption: "On the floor",
+        correctAnswer: "#fourth-option"
+    },
+   
+    {
+        // questionNumber: "History-4",
+        question: "Who is your bestie?",
+        firstOption: "Pragya",
+        secondOption: "Misha",
+        thirdOption: "Anannya",
+        fourthOption: "Yohaan",
+        correctAnswer: "#fourth-option"
+    },
+   
+  
+    {
+        // questionNumber: "Language-2",
+        question: "What song do Anannya and Yohaan keep singing?",
+        firstOption: "Closer",
+        secondOption: "Don't let me down",
+        thirdOption: "Colors",
+        fourthOption: "Starboy",
+        correctAnswer: "#first-option"
+    },
+    
+  
+
+]
